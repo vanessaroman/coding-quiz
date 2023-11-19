d31b44ced76d67c71e3da0cddcbc1f32b85d82eb
vanessaroman <103296228+vanessaroman@users.noreply.github.com>
2023-11-19 00:16:53 -0700

    main card updated and topic buttons added

--- coding-quiz/src/App.js
+++ coding-quiz/src/App.js
@@ -8,9 +8,17 @@ import {
   Code,
   Grid,
   theme,
+  Heading,
+  Stack,
+  StackDivider,
+  GridItem,
+  Button,
+
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
+
 
 function App() {
   return (
@@ -19,19 +27,54 @@ function App() {
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            
+          <Card>
+
+  <CardHeader>
+    <Heading size='md'>Client Report</Heading>
+  </CardHeader>
+
+  <CardBody>
+    
+  <Grid templateColumns='repeat(2, 1fr)' gap={6}>
+
+  <GridItem w='100%'  bg='blue.500'></GridItem>
+     
+  <GridItem w='100%'>
+
+      <Box py='5px'>
+      <Button colorScheme='teal' variant='solid'>
+    
+          View a summary of all your clients over the last month.
+        
+      </Button>
+      </Box>
+  
+      <Box py='5px'>
+      <Button colorScheme='teal' variant='solid'>
+          Check out the overview of your clients.
+        </Button>
+      </Box>
+
+      <Box py='5px'>
+        <Button colorScheme='teal' variant='solid'>
+        
+          See a detailed analysis of all your business clients.
+        </Button>
+      </Box>
+
+  </GridItem>
+
+  
+  
+    </Grid>
+  </CardBody>
+</Card>
+          
+
+
+
+
           </VStack>
         </Grid>
       </Box>
